412b711da076b09bea45c8fbf1563cd632f2801b
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2026-01-27 22:58:01 -0800

    migrate book and review data

---
+++ app/Models/Book.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    //
+    
+    // public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
+    // {
+    //     return $query->where('reviews_count', '>=', $minReviews);
+    // }
+}
